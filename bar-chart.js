9a895ea847cb6c24954ed26da8ae8543c12228d0
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2026-02-10 20:11:16 +0000

    yAxis amends

--- bar-chart.js
+++ bar-chart.js
@@ -1,35 +1,7 @@
-function RoundedMax(a) {
-    const mx = Math.max(...a);
-    if (mx == 0) {return 0};
-    const size = Math.floor(Math.log10(Math.abs(mx)));
-    const magnitude = Math.pow(10, size);
-    const yMax = Math.ceil(mx / magnitude) * magnitude;
-    return yMax;
-}
-
-function RoundedMin(a) {
-    const mn = Math.min(...a);
-    if (mn == 0) {return 0};
-    const size = Math.floor(Math.log10(Math.abs(mn)));
-    const magnitude = Math.pow(10, size);
-    const yMin = Math.floor(mn / magnitude) * magnitude;
-    return yMin;
-}
-
-const array = [20, 45, 43, 56, 2, 98];
-const roundedMax = RoundedMax(array);
-console.log('Rounded Max:', roundedMax); // Output: Rounded Max: 100
-
-const roundedMin = RoundedMin(array);
-console.log('Rounded Min:', roundedMin); // Output: Rounded Min: 0
-
 class BarChart extends HTMLElement {
   constructor() {
     super();
     this.data = null;
-    this.maxValue = null;
-    this.yAxisMaxValue = null;
-    this.afterDecimal = null;
   }
   
   connectedCallback() {
@@ -38,7 +10,7 @@ class BarChart extends HTMLElement {
     this.chartInner = this.querySelector('[data-inner]');
     this.bars = this.querySelector('[data-bars]');
     this.yAxis = this.querySelector('[data-y-axis]');
-    this.tickMarks = this.getAttribute('data-tick-marks');
+    this.xAxis = this.querySelector('[data-x-axis]');
     this.showPatterns = this.getAttribute('data-show-patterns') === 'true';
     this.legend = this.querySelector('[data-legend]');
     if (this.data && this.data.values) {
@@ -56,61 +28,77 @@ class BarChart extends HTMLElement {
       return;
     }
     this.data = data;
+    this.setYAxisValues();
+
+    if (this.minValue < 0) {
+      this.bars.style.setProperty('--positive-offset-padding', `${(Math.abs(this.yAxisMinValue) / this.yAxisRange) * 100}cqh`);
+      this.bars.style.setProperty('--negative-offset-padding', `${(Math.abs(this.yAxisMaxValue) / this.yAxisRange) * 100}cqh`);
+    } else {
+      this.bars.style.removeProperty('--positive-offset-padding');
+      this.bars.style.removeProperty('--negative-offset-padding');
+    }
     this.render();
   }
 
-  
-  // setYAxisAbove1() {
-  //   const roundedMax = Math.ceil(this.maxValue);
-  //   const numOfDigits = roundedMax.toString().length;
-  //   const firstDigit = parseInt(roundedMax.toString()[0], 10);
-  //   const newFirstDigit = firstDigit === 1 && roundedMax.toString()[1] === '0' ? 11 : firstDigit + 1;
-  //   const updatedNumOfDigits = newFirstDigit === 10 ? numOfDigits + 1 : numOfDigits;
-  //   const scaleString = newFirstDigit.toString().padEnd(updatedNumOfDigits, '0');
-  //   this.yAxisMaxValue = parseInt(scaleString, 10);
-  // }
+  setYAxisValues() {
+    this.maxValue = Math.max(...this.data.values.map((item) => {
+      return Math.max(...item.dataSets.map(obj => Object.values(obj)[0]));
+    }));
+    this.minValue = Math.min(...this.data.values.map((item) => {
+      return Math.min(...item.dataSets.map(obj => Object.values(obj)[0]));
+    }));
+    this.valueRange = this.maxValue - this.minValue;
+    const size = Math.floor(Math.log10(Math.abs(this.valueRange)));
+    this.magnitude = Math.pow(10, size);
 
-  // setYAxisBelow1() {
-  //   const afterDecimal = String(this.maxValue).split('.')[1];
-  //   let zerosAfterDecimal = 0;
-  //   let significantDigit = null;
-  //   for (const char of afterDecimal) {
-  //     if (char === '0') {
-  //       zerosAfterDecimal += 1;
-  //     } else {
-  //       significantDigit = char;
-  //       break;
-  //     }
-  //   }
-  //   this.afterDecimal = zerosAfterDecimal + 1;
-  //   const newSignificantDigit = parseInt(significantDigit, 10) + 1;
-  //   const updatedZerosAfterDecimal = newSignificantDigit > 9 ? zerosAfterDecimal - 1 : zerosAfterDecimal;
-  //   this.yAxisMaxValue = parseFloat(`0.${'0'.repeat(updatedZerosAfterDecimal)}${newSignificantDigit}`);
-  // }
+    const tickMultiplierInitial = this.valueRange / this.magnitude;
 
-  // We want the Y-axis max value to be a rounded number just above the actual max value
-  setYAxisMaxValue() {
-    const size = Math.floor(Math.log10(Math.abs(this.maxValue)));
-    const magnitude = Math.pow(10, size);
-    const yMax = Math.ceil(this.maxValue / magnitude) * magnitude;
-    this.yAxisMaxValue = yMax;
+    switch (true) {
+      case tickMultiplierInitial <= 2:
+        this.tickMultiplier = this.magnitude / 5;
+        break;
+      case tickMultiplierInitial <= 5:
+        this.tickMultiplier = this.magnitude / 2;
+        break;
+      default:
+        this.tickMultiplier = this.magnitude;
+    }
+
+    if (this.maxValue % this.tickMultiplier !== 0) {
+      this.yAxisMaxValue = Math.ceil(this.maxValue / this.tickMultiplier) * this.tickMultiplier;
+    } else {
+      this.yAxisMaxValue = this.maxValue;
+    }
+    if (this.minValue % this.tickMultiplier !== 0) {
+      this.yAxisMinValue = Math.floor(this.minValue / this.tickMultiplier) * this.tickMultiplier;
+    } else {
+      this.yAxisMinValue = this.minValue;
+    }
+    this.yAxisRange = this.yAxisMaxValue - this.yAxisMinValue;
   }
-  // setYAxisMaxValue() {
-  //   if (this.maxValue > 1) {
-  //     this.setYAxisAbove1();
-  //   } else {
-  //     this.setYAxisBelow1();
-  //   }
-  // }
 
-  generateBars() {
+  generateBars(item) {
+    const barsHtml = item.dataSets.map((dataSet, index) => {
+      const value = Object.values(dataSet)[0];
+      const itemClass = value >= 0 ? '' : 'chart__item--negative';
+      const patternClass = this.showPatterns ? 'pattern-' + (index + 1) : '';
+      const barHeight = value >=0 ? (value / this.yAxisMaxValue) * 100 : (Math.abs(value) / Math.abs(this.yAxisMinValue)) * 100;
+      const barColor = this.data.colors[index];
+      const title = `${Object.keys(dataSet)[0]}: ${value}`;
+      return `
+        <li class="chart__item ${itemClass}">
+          <div class="chart__bar ${patternClass}" style="--transition-height: ${barHeight}%; --bar-color: ${barColor};" title="${title}"></div>
+        </li>
+      `;
+    }).join('');
+    return barsHtml;
+  }
+
+  generateBarGroups() {
     return this.data.values.map((item, index) => `
-      <div class="chart__bar-group" id="bar-${index}">
-        ${item.dataSets.map((dataSet, index) => `
-          <div class="chart__bar ${this.showPatterns ? 'pattern-' + (index + 1) : ''}" style="--transition-height: ${(Object.values(dataSet)[0] / this.yAxisMaxValue) * 100}%; --bar-color: ${this.data.colors[index]};" title="${Object.keys(dataSet)[0]}:${Object.values(dataSet)[0]}"></div>
-        `).join('')}
-        <span class="chart__bar-label">${item.label}</span>
-      </div>
+      <ul class="chart__bar-group" id="bar-${index}">
+        ${this.generateBars(item)}
+      </ul>
     `).join('');
   }
 
@@ -121,39 +109,48 @@ class BarChart extends HTMLElement {
     });
   }
 
+  generateXAxis() {
+    return this.data.values.map((item) => `
+      <div class="chart__y-axis-label">
+        ${item.label}
+      </div>
+    `).join('');
+  }
+
   generateYAxis() {
     let lines = ``;
-    const numberOfTicks = Number(this.tickMarks) || 10;
+    let numberOfTicks = 0;
     const decimalPlaces = this.yAxisMaxValue <= 5 ? 2 : 0;
-    for (let i = numberOfTicks; i > 0; i--) {
-      const lineItem = `<div class="chart__y-axis-line">
-        <span class="chart__y-axis-value">${(this.yAxisMaxValue / numberOfTicks * i).toFixed(decimalPlaces)}</span></div>`;
+    for (let i = this.yAxisMaxValue; i >= this.yAxisMinValue; i -= this.tickMultiplier) {
+      const oddEvenClass = (numberOfTicks % 2 === 0) ? 'even' : 'odd';
+      const lineItem = `<div class="chart__y-axis-line ${oddEvenClass}">
+        <span class="chart__y-axis-value">${(i).toFixed(decimalPlaces)}</span></div>`;
       lines += lineItem;
-      if (i == 1) {
-        lines += `<div class="chart__y-axis-zero">
-          <span class="chart__y-axis-zero-value">0</span></div>`;
-      }
+      numberOfTicks++;
     }
     this.yAxis.style.setProperty('--number-of-ticks', numberOfTicks);
     return lines;
   }
 
   generateLegend() {
-     console.log('Generating legend');
-     return this.data.values[0].dataSets.map((item, index) => `<div class="chart__legend-item">
-      <span class="chart__legend-identifier ${this.showPatterns ? 'pattern-' + (index + 1) : ''}" style="--bar-color: ${this.data.colors[index]}"></span>
-      <span class="chart__legend-label">${Object.keys(item)[0]}</span></div>`).join('');
+    const legendHtml = this.data.values[0].dataSets.map((item, index) => {
+      const patternClass = this.showPatterns ? 'pattern-' + (index + 1) : '';
+      const barColor = this.data.colors[index];
+      const label = Object.keys(item)[0];
+      return `<div class="chart__legend-item">
+        <span class="chart__legend-identifier ${patternClass}" style="--bar-color: ${barColor}"></span>
+        <span class="chart__legend-label">${label}</span></div>`;
+
+    }).join('');
+    return legendHtml;
   }
 
   render() {
-    this.maxValue = Math.max(...this.data.values.map((item) => {
-      return Math.max(...item.dataSets.map(obj => Object.values(obj)));
-    }));
-    this.setYAxisMaxValue();
     const numberOfBars = this.data.values.length;
-    this.style.setProperty('--number-of-bars', numberOfBars);
-    this.bars.innerHTML = this.generateBars();
+    this.style.setProperty('--number-of-bar-groups', numberOfBars);
+    this.bars.innerHTML = this.generateBarGroups();
     this.yAxis.innerHTML = this.generateYAxis();
+    this.xAxis.innerHTML = this.generateXAxis();
     if (this.getAttribute('data-show-legend') === 'true') {
       this.legend.innerHTML = this.generateLegend();
     }
@@ -171,13 +168,13 @@ const chart = document.getElementById('barChart');
 chart.chartData = {
   values: [
     { label: 'Monday',
-      dataSets: [ {a: 0.1}, {b: 0.78}, {c: 0.45}, {d: 0.50}, {e: 0.60}, {f: 0.9} ]
+      dataSets: [ {a: 0.2}, {b: -0.1}, {c: -0.3}, {d: 0.4}, {e: 0.60}, {f: 0.9} ]
     },
     { label: 'Tuesday',
       dataSets: [ {a: 0.56}, {b: 0.23}, {c: 0.89}, {d: 0.40}, {e: 0.70}, {f: 0.80} ]
     },
     { label: 'Wednesday',
-      dataSets: [ {a: 0.74}, {b: 0.97}, {c: 0.65}, {d: 0.50}, {e: 0.80}, {f: 0.86} ]
+      dataSets: [ {a: 0.74}, {b: 0.97}, {c: 0.65}, {d: -0.48}, {e: 0.80}, {f: 0.86} ]
     },
   ],
   colors: ['#25C7D9', '#F2D338', '#F2622E', '#03A678', '#8E44AD', '#F27457']
@@ -185,7 +182,7 @@ chart.chartData = {
 const updateButton = document.getElementById('updateButton');
 updateButton.addEventListener('click', () => {
   const newData = Array.from({ length: 5 }, () => {
-    return Array.from({ length: 4 }, () => Math.floor(Math.random() * 100));
+    return Array.from({ length: 4 }, () => Math.floor(Math.random() * 50));
   });
   chart.chartData = {
     values: newData.map((value, index) => ({
